0952cffc837d3669cf3ad2e50a5531067fe133b1
agra <alex@swipelab.co>
2026-03-24 10:00:47 +0200

    1.10.0

--- test/keyboard_height_test.dart
+++ test/keyboard_height_test.dart
@@ -1,29 +1,60 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:keyboard_height/keyboard_height.dart';
-import 'package:keyboard_height/keyboard_height_platform_interface.dart';
-import 'package:keyboard_height/keyboard_height_method_channel.dart';
+import 'package:keyboard_height/src/platform_interface/keyboard_height_platform_interface.dart';
+import 'package:keyboard_height/src/keyboard_height_mobile.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockKeyboardHeightPlatform
-    with MockPlatformInterfaceMixin
-    implements KeyboardHeightPlatform {
+class MockKeyboardHeightPlatform extends KeyboardHeightPlatform
+    with MockPlatformInterfaceMixin {
+  final StreamController<Map<String, dynamic>> _controller =
+      StreamController<Map<String, dynamic>>.broadcast();
+
   @override
-  Future<String?> getPlatformVersion() => Future.value('42');
+  Stream<Map<String, dynamic>> keyboardHeightEventStream() {
+    return _controller.stream;
+  }
+
+  void emitEvent(Map<String, dynamic> event) {
+    _controller.add(event);
+  }
+
+  void dispose() {
+    _controller.close();
+  }
 }
 
 void main() {
   final KeyboardHeightPlatform initialPlatform =
       KeyboardHeightPlatform.instance;
 
-  test('$MethodChannelKeyboardHeight is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelKeyboardHeight>());
+  test('$KeyboardHeightMobile is the default instance', () {
+    expect(initialPlatform, isInstanceOf<KeyboardHeightMobile>());
   });
 
-  test('getPlatformVersion', () async {
-    KeyboardHeight keyboardHeightPlugin = KeyboardHeight();
-    MockKeyboardHeightPlatform fakePlatform = MockKeyboardHeightPlatform();
+  test('keyboardHeightEventStream returns a stream', () {
+    final fakePlatform = MockKeyboardHeightPlatform();
     KeyboardHeightPlatform.instance = fakePlatform;
 
-    expect(await keyboardHeightPlugin.getPlatformVersion(), '42');
+    final stream = fakePlatform.keyboardHeightEventStream();
+    expect(stream, isA<Stream<Map<String, dynamic>>>());
+
+    fakePlatform.dispose();
+  });
+
+  test('keyboardHeightEventStream emits events', () async {
+    final fakePlatform = MockKeyboardHeightPlatform();
+    KeyboardHeightPlatform.instance = fakePlatform;
+
+    final event = {'height': 300.0, 'duration': 250, 'open': true};
+
+    expectLater(
+      fakePlatform.keyboardHeightEventStream(),
+      emits(event),
+    );
+
+    fakePlatform.emitEvent(event);
+
+    fakePlatform.dispose();
   });
 }
